30545e19cf77d165702e6fed3de546a55d5fe23a
chatgptree <chatgptree.au@gmail.com>
2025-02-04 14:59:01 +1100

    Update messageboard.js

--- messageboard/messageboard.js
+++ messageboard/messageboard.js
@@ -56,7 +56,7 @@ class TreeMessageBoard {
         console.log('Starting initial load for date:', today);
         
         // Load last 7 days
-        for (let i = 0; i < 7; i++) {
+        for (let i = 0; i < 2; i++) {
             const date = new Date(today);
             date.setDate(date.getDate() - i);
             console.log(`Loading day ${i}:`, date.toISOString().split('T')[0]);
